30c77d6971616249f83c14b5a3823f1cf6967ff5
AJ Slater <aj@slater.net>
2019-10-29 15:21:48 +0530

    fix text

--- mkvstrip.py
+++ mkvstrip.py
@@ -375,7 +375,7 @@ def main(params=None):
                         help="If specified, defines subtitle languages to retain. See description of --language "
                              "for syntax.")
     parser.add_argument("-v", "--verbose", action="store_true",
-                        default=False, help="Verbose output about.")
+                        default=False, help="Verbose output.")
 
     # Parse the list of given arguments
     globals()["cli_args"] = parser.parse_args(params)
